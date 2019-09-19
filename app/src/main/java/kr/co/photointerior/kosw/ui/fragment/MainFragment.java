45b59695f4844d705756ff8101aa44bee7f2711b
ilovebeer <seo1108@gmail.com>
2019-09-20 02:36:10 +0900

    서비스등록 로직 수정

--- app/src/main/java/kr/co/photointerior/kosw/ui/fragment/MainFragment.java
+++ app/src/main/java/kr/co/photointerior/kosw/ui/fragment/MainFragment.java
@@ -1275,7 +1275,7 @@ public class MainFragment extends BaseFragment {
             title = "[자동측정]";
         }*/
 
-        builder.setContentTitle("[랭킹: + ranking + ]")
+        builder.setContentTitle("[랭킹:" + ranking + "]")
                 .setContentText(floor + "F / " + cal + "kcal / " + sec + "sec")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ic_floor))
